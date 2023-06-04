8a9aa6d26eb501917f88fa12f97988fbba10d29c
Santiago Luis Ocón <ocon.santiago@gmail.com>
2023-06-04 17:16:55 -0300

    Add main app files

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
+import { Main } from './src/components/Main'
+import expoConstants from 'expo-constants'
 
-export default function App() {
+export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Main />
+      <StatusBar style="light" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#181818',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    paddingTop: expoConstants.statusBarHeight
+  }
+})
